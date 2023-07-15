a1b06fcf0a12765dde9bce360d9b4afd077a2c4e
Luis Balbás <30774937+lbalbas@users.noreply.github.com>
2023-07-15 17:53:41 -0400

    Start api

--- src/server/api/routers/blogposts.ts
+++ src/server/api/routers/blogposts.ts
@@ -5,7 +5,7 @@ import {
   protectedProcedure,
 } from "~/server/api/trpc";
 
-export const exampleRouter = createTRPCRouter({
+export const blogPostsRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
     .query(({ input }) => {
@@ -15,10 +15,13 @@ export const exampleRouter = createTRPCRouter({
     }),
 
   getAll: publicProcedure.query(({ ctx }) => {
-    return {msg: "greetings"};
+    console.log("hey");
+    return { msg: "greetings" };
   }),
 
-  getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!";
-  }),
+  publish: publicProcedure
+    .input(z.object({ title: z.string(), content: z.string() }))
+    .mutation(({ ctx, input }) => {
+      return { post: input.content };
+    }),
 });
