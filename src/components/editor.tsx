86a89a12349cf8c8ba14798085391f235fdfea46
Luis Balbás <30774937+lbalbas@users.noreply.github.com>
2023-07-17 17:32:26 -0400

    Changes to editor, expand content limit, add html parser

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,59 +1,36 @@
 import dynamic from "next/dynamic";
-import { useState } from "react";
+import { useState, useRef, useCallback } from "react";
 import { api } from "~/utils/api";
 
-const QuillNoSSRWrapper = dynamic(import("react-quill"), {
-  ssr: false,
-  loading: () => <p>Loading ...</p>,
-});
-
-const modules = {
-  toolbar: [
-    [{ header: "1" }, { header: "2" }, { font: [] }],
-    [{ size: [] }],
-    ["bold", "italic", "underline", "strike", "blockquote"],
-    [
-      { list: "ordered" },
-      { list: "bullet" },
-      { indent: "-1" },
-      { indent: "+1" },
-    ],
-    ["link", "image", "video"],
-    ["clean"],
-  ],
-  clipboard: {
-    // toggle to add extra line breaks when pasting HTML:
-    matchVisual: false,
-  },
+type QuillProps = {
+  forwardedRef: React.Ref<any>;
+  [key: string]: any;
 };
-/*
- * Quill editor formats
- * See https://quilljs.com/docs/formats/
- */
-const formats = [
-  "header",
-  "font",
-  "size",
-  "bold",
-  "italic",
-  "underline",
-  "strike",
-  "blockquote",
-  "list",
-  "bullet",
-  "indent",
-  "link",
-  "image",
-  "video",
-];
+
+const QuillNoSSRWrapper = dynamic(
+  async () => {
+    const { default: RQ } = await import("react-quill");
+    const Component = ({ forwardedRef, ...props }: QuillProps) => (
+      <RQ ref={forwardedRef} {...props} />
+    );
+    Component.displayName = "QuillNoSSRWrapper";
+    return Component;
+  },
+  { ssr: false }
+);
+
+type QuillInstance = {
+  getEditor: () => any;
+  theme: {
+    tooltip: any;
+  };
+};
 
 export default function Editor() {
   const [value, setValue] = useState("");
   const [title, setTitle] = useState("");
+  const quillRef = useRef<QuillInstance | null>(null);
   const { mutate, isLoading: isPosting } = api.blogs.publish.useMutation({
-    onSuccess: () => {
-      console.log("success");
-    },
     onError: (e) => {
       const errorMessage = e.data?.zodError?.fieldErrors.content;
       if (errorMessage && errorMessage[0]) {
@@ -63,10 +40,80 @@ export default function Editor() {
       }
     },
   });
+
+  const imageHandler = useCallback(() => {
+    if (quillRef.current) {
+      const quill = quillRef.current.getEditor();
+      const tooltip = quill.theme.tooltip;
+      const originalSave = tooltip.save;
+      const originalHide = tooltip.hide;
+
+      tooltip.save = function () {
+        const range = this.quill.getSelection(true);
+        const value = this.textbox.value;
+        if (value) {
+          this.quill.insertEmbed(range.index, "image", value, "user");
+        }
+      };
+
+      tooltip.hide = function () {
+        tooltip.save = originalSave;
+        tooltip.hide = originalHide;
+        tooltip.hide();
+      };
+
+      tooltip.edit("image");
+      tooltip.textbox.placeholder = "Embed URL";
+    }
+  }, []);
+
+  const modules = {
+    toolbar: {
+      container: [
+        [{ header: "1" }, { header: "2" }, { font: [] }],
+        [{ size: [] }],
+        ["bold", "italic", "underline", "strike", "blockquote"],
+        [
+          { list: "ordered" },
+          { list: "bullet" },
+          { indent: "-1" },
+          { indent: "+1" },
+        ],
+        ["link", "image", "video"],
+        ["clean"],
+      ],
+      handlers: {
+        image: imageHandler,
+      },
+    },
+    clipboard: {
+      // toggle to add extra line breaks when pasting HTML:
+      matchVisual: false,
+    },
+  };
+
+  const formats = [
+    "header",
+    "font",
+    "size",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "video",
+  ];
+
   return (
     <div>
       <input onChange={(e) => setTitle(e.target.value)} type="Text" />
       <QuillNoSSRWrapper
+        forwardedRef={quillRef}
         placeholder="Start writing your Blog Post here"
         modules={modules}
         value={value}
