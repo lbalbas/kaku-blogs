27d4a923b887d39ef7f291bc9d5a02dd769d967d
Luis Balbás <30774937+lbalbas@users.noreply.github.com>
2023-07-22 16:56:13 -0400

    Add user's draft list

---
+++ src/pages/drafts/index.tsx
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import { api } from "~/utils/api";
+import Link from "next/link";
+import LoadingBlock from "~/components/loading";
+const Drafts = () => {
+  const { data: drafts, isLoading } = api.drafts.getAllByCurrentUser.useQuery(
+    {},
+    {
+      onError: (e) => {
+        console.log(e);
+      },
+    }
+  );
+  if (isLoading) return <LoadingBlock size={32} />;
+  return (
+    <div className="flex flex-col items-center">
+      <Head>
+        <title>Drafts</title>
+      </Head>
+      {drafts.length == 0 ? (
+        <div>No Drafts yet!</div>
+      ) : (
+        drafts.map((draft) => {
+          return (
+            <Link key={draft.id} href={`/drafts/${draft.id}`}>
+              {draft.title}
+            </Link>
+          );
+        })
+      )}
+    </div>
+  );
+};
+
+export default Drafts;
