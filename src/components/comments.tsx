e3b015524a8c0b66b49936651a7553324fb15c35
Luis Balbás <30774937+lbalbas@users.noreply.github.com>
2023-08-07 16:58:58 -0400

    asdf

--- src/components/comments.tsx
+++ src/components/comments.tsx
@@ -1,7 +1,28 @@
 import LoadingBlock from "./loading";
 import { api } from "~/utils/api";
-import Link from 'next/link';
 import {useState} from 'react';
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import Link from "next/link";
+
+dayjs.extend(relativeTime);
+
+interface Comment {
+  id: string;
+  content: string;
+  userId: string;
+  blogPostId: string;
+  createdAt: Date;
+  parentCommentId: string | null;
+  user: {
+    id: string;
+    name: string | null;
+    email: string | null;
+    emailVerified: Date | null;
+    image: string | null;
+  };
+  childComments?: Comment[];
+}
 
 const Comments = (props: {post: string}) => {
 	const [comment, setComment] = useState("");
@@ -17,15 +38,22 @@ const Comments = (props: {post: string}) => {
 
 	if(!data) return <div>Something went wrong!</div>
 
-	const renderParentComments = () => {
+	const renderComments = (comments: Comment[]) => {
 		return data.map(comment => {
 			return (
-			<div className="flex flex-col">
+			<div key={comment.id} className="flex flex-col">
+				<div className="flex gap-4 items-center">
 				<Link href={`/user/${comment.user.id}`} className="flex gap-1 items-center">
 					<img className="rounded-full h-6 w-6" src={comment.user.image!} alt="Profile picture" />
 					<span className="font-bold text-sm">{comment.user.name}</span>
 				</Link>
+				<span className="text-xs italic text-slate-500"></span>
+				</div>
 				<p>{comment.content}</p>
+				<div>
+					<button>Reply</button>
+				</div>
+				{(comment.childComments && comment.childComments.length > 0) ? (<div className="pl-2">{renderComments(comment.childComments)}</div>) : ""}
 			</div>)
 		})
 	}
@@ -36,9 +64,9 @@ const Comments = (props: {post: string}) => {
 				<textarea maxLength={300} minLength={1} value={comment} onChange={(e)=>setComment(e.target.value)} />
 				<button onClick={()=>mutate({content: comment, post})}>Comment</button>
 			</div>
-			{data.length == 0 ? (<p>No comments yet, be the first one!</p>) : renderParentComments(data)}
+			{data.length == 0 ? (<p>No comments yet, be the first one!</p>) : renderComments(data)}
 		</div>
 	)
-}
+} 
 
 export default Comments;
--- src/server/api/routers/comments.ts
+++ src/server/api/routers/comments.ts
@@ -3,18 +3,18 @@ import { createTRPCRouter, publicProcedure, protectedProcedure } from "~/server/
 
 export const commentsRouter = createTRPCRouter({
 	getCommentsByPost: publicProcedure.input(z.object({post: z.string()})).query(async({ctx, input})=>{
-		const comments = ctx.prisma.comment.findMany({
+		const comments = await ctx.prisma.comment.findMany({
 			where: {
 				blogPostId: input.post,
 			},
 			include: {
 				user: true,
-				parentComment: true,
-				childComments: true,
+			},
+			orderBy: {
+			    createdAt: 'asc',
 			}
 		})
-
-		return comments;
+		return commentsToTree(comments);
 	}),
 	makeComment: protectedProcedure.input(z.object({post: z.string(), content: z.string().min(1)})).mutation(async({ctx, input}) =>{
 		return await ctx.prisma.comment.create({
@@ -26,3 +26,43 @@ export const commentsRouter = createTRPCRouter({
 		})
 	})
 })
+
+interface Comment {
+  id: string;
+  content: string;
+  userId: string;
+  blogPostId: string;
+  parentCommentId: string | null;
+  createdAt: Date;
+  user: {
+    id: string;
+    name: string | null;
+    email: string | null;
+    emailVerified: Date | null;
+    image: string | null;
+  };
+  childComments?: Comment[];
+}
+
+function commentsToTree(comments: Comment[]) {
+  let commentMap: { [key: string]: Comment } = {};
+
+  // make a map for fast lookup
+  comments.forEach((comment) => {
+    comment.childComments = []; // add children placeholder
+    commentMap[comment.id] = comment;
+  });
+
+  // connect child comments to their parent and gather root comments
+  let roots: Comment[] = [];
+  comments.forEach((comment) => {
+    if (!comment.parentCommentId) { // is a root comment
+      roots.push(comment);
+    } else {
+      // it is guaranteed to exist
+      commentMap[comment.parentCommentId]!.childComments?.push(comment);
+    }
+  });
+
+  return roots;
+}
